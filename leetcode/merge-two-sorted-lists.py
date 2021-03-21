029944f59cdb6293092515302e1b51c91f107f5f
hanzhang <qingeneral@gmail.com>
2021-03-21 20:17:56 +0800

    update merge two list code

--- leetcode/merge-two-sorted-lists.py
+++ leetcode/merge-two-sorted-lists.py
@@ -2,44 +2,28 @@ from node import ListNode
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        newhead = None
-        returnhead = None
-        if l1 and l2:
-            if l1.val < l2.val:
-                newhead = l1
-                l1 = l1.next
-            else:
-                newhead = l2
-                l2 = l2.next
-        elif l1:
-            return l1
-        elif l2:
+        if l1 is None:
             return l2
-        else:
-            return None
+        if l2 is None:
+            return l1
+
+        newhead = ListNode(-1, None)
 
         returnhead = newhead
         
-        while True:
-            if l1 and l2:
-                if l1.val < l2.val:
-                    newhead.next = l1
-                    l1 = l1.next
-                    newhead = newhead.next
-                else:
-                    newhead.next = l2
-                    l2 = l2.next
-                    newhead = newhead.next
+        while l1 and l2:
+            if l1.val < l2.val:
+                newhead.next = l1
+                l1 = l1.next
             else:
-                break;
+                newhead.next = l2
+                l2 = l2.next
+            newhead = newhead.next
 
-        while l1:
+        if l1:
             newhead.next = l1
-            l1 = l1.next
-            newhead = newhead.next
-        while l2:
-            newhead.next = l2
-            l2 = l2.next
-            newhead = newhead.next
 
-        return returnhead
+        if l2:
+            newhead.next = l2
+
+        return returnhead.next
